35c5554fb3b0c9670eb65475a4df01b8ef0db450
Hack Reactor Students <github@hackreactor.com>
2013-06-20 12:53:23 -0700

    Basic set up of Prototypal Queue Problem

--- src/prototypal/queue.js
+++ src/prototypal/queue.js
@@ -1,3 +1,32 @@
+var queueMethods = {
+  enqueue: function (value){
+    this.storage[this.last] = value;
+    this.last++;
+    this.length++;
+  },
+  dequeue:  function(){
+    var temp = this.storage[this.first];
+    this.first++;
+    this.length -= 1;
+    return temp;
+  },
+  size: function(){
+    return this.length;
+  }
+};
+
 var Queue = function() {
   // Hey! Copy your code from src/functional-shared/queue.js and paste it here
+
+   var queue = {
+    first : 0,
+    last : 0,
+    length : 0,
+    storage : {}
+  };
+  _.extend(queue, queueMethods);
+  return queue;
+
 };
+
+
