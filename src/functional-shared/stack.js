2e6300aaa65eaa2eb5d324aa76e6c76df0f5032a
Hack Reactor Students <github@hackreactor.com>
2013-06-19 19:25:30 -0700

    Started Second

--- src/functional-shared/stack.js
+++ src/functional-shared/stack.js
@@ -1,3 +1,30 @@
 var makeStack = function(){
-  // Hey! Copy your code from src/functional/stack.js and paste it here
+  // Use an object with numeric keys to store values
+  var storage = {};
+
+  var size = 0; // Hint: set an initial value here
+
+  // Implement the methods below
+  var stack = {};
+
+  var methodHolder = {
+    push: function (value){
+      this.size += 1;
+      storage[size] = value;
+    },
+
+    pop:  function(){
+      var tmp = storage[size];
+      delete storage[size];
+      this.size--; 
+      return tmp;
+    },
+
+    size: function(){
+      return thissize;
+    }
+
+      // _.extend(methodHolder)
+  };
+  return stack;
 };
